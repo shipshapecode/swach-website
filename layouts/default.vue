13ec2771a47453ab4a3eed68090d16a30c90d8ee
Robert Wagner <rwwagner90@gmail.com>
2019-12-13 21:29:06 -0500

    Initial Nuxt setup

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="flex h-12 items-center justify-center p-6 shadow w-full">
+      <div class="font-bold max-w-6xl text-lg w-full">
+        Swach
+      </div>
+    </div>
+
+    <main>
+      <nuxt />
+    </main>
+  </div>
+</template>
+
+<style>
+@font-face {
+  font-family: 'Muli';
+  src: url('/fonts/muli-variable.ttf') format('truetype');
+  font-weight: 1 999;
+}
+
+html {
+  font-family: Muli, 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex justify-center text-center w-full">
+    <div class="max-w-5xl w-full">
+      <h1 class="font-black p-4 text-3xl md:text-6xl">
+        A robust color management tool for the modern age.
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
