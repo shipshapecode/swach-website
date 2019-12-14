ad63d4f9e00ff6d84e18a1ed2bd5f5d35cc9999a
Robert Wagner <rwwagner90@gmail.com>
2019-12-14 12:00:18 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'A robust color management tool for the modern age.'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
