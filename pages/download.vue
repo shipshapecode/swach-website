fbf205cb6a9483595849f5978a72222fa53c5d0c
Robert Wagner <rwwagner90@gmail.com>
2020-06-14 21:12:23 -0400

    Update download.vue

--- pages/download.vue
+++ pages/download.vue
@@ -44,11 +44,15 @@
 </template>
 
 <script>
+import { generateMeta } from '~/utils/meta';
+
 export default {
   head() {
-    return {
-      title: 'Download'
-    };
+    const title = 'Download';
+    const description =
+      'Download Swach for free for macOS, Windows, and Linux.';
+    const url = 'https://swach.io/download/';
+    return generateMeta(title, description, url);
   }
 };
 </script>
