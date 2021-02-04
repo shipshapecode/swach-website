eb81fceb7d72341a110abdd0caa153692e32e743
Robert Wagner <rwwagner90@gmail.com>
2021-02-04 15:35:36 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -5,18 +5,22 @@ const { Router } = require('@xdn/core/router');
 const { nuxtRoutes } = require('@xdn/nuxt');
 
 module.exports = new Router()
+  .prerender([
+    // HTML pages
+    { path: '/' },
+    { path: '/about/' },
+    { path: '/docs/' },
+    { path: '/docs/contrast-checker/' },
+    { path: '/docs/picker/' },
+    { path: '/docs/shortcuts/' },
+    { path: '/download/' },
+    { path: '/download/linux/' },
+    { path: '/releases/' }
+  ])
   .match('/service-worker.js', ({ serviceWorker }) => {
     serviceWorker('.nuxt/dist/client/service-worker.js');
   })
   // .match('/sitemap.xml', ({ serveStatic }) => {
   //   serveStatic('dist/sitemap.xml');
   // })
-  .use(nuxtRoutes)
-  .prerender([
-    // HTML pages
-    { path: '/' },
-    { path: '/about/' },
-    { path: '/download/' },
-    { path: '/download/linux/' },
-    { path: '/releases/' }
-  ]);
+  .use(nuxtRoutes);
